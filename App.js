804823655fd5e8bcead871eae056b312cf083d4b
Maheshwar Ugale <maheshwarugale401@gmail.com>
2024-03-31 11:36:46 +0530

    components

--- App.js
+++ App.js
@@ -1,28 +1,61 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom"
 
-// React element
 
-const heading = React.createElement("h1", { id: "heading" }, "I am a React!");
-
-// jsx - html-like syntax 
-// and jsxHeading is react element is an object
-const jsxHeading = (<h1 id="heading">Namaste React using jsx!</h1>)
-
-// react functional component
-
-const Title = () => {
-    return <h1 className="head">Heading Mahesh!</h1>
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-cantainer">
+                <img className="logo" src="https://as2.ftcdn.net/v2/jpg/06/62/27/35/1000_F_662273587_FUApAaMvV1hBiKXTXbWaxbhCmKI1p6QX.jpg" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
 }
-const HeadingComponent = () => {
-    return <div>
-        <Title />
-        <h1 className="heading">Namaste mahesh!</h1>
-    </div>
+const styleCard = {
+    backgroundColor: "#f0f0f0"
 }
 
+const RestorantCard = () => {
+    return (
+        <div className="res-card" style={styleCard}>
+            <div>
+                <img className="cardImg" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/akbvvc2wh0wgei00vax4" />
+            </div>
+            <h3>Meghna Foods</h3>
+            <h4>Biryani, North Indian, Asian</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minuts</h4>
+        </div>
+    )
+}
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestorantCard />
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header></Header>
+            <Body></Body>
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
-console.log(heading);
+root.render(<AppLayout />);
 
