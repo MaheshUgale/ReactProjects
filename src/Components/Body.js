533f0b401990c12765c30e2901a325eefd0353db
Maheshwar Ugale <maheshwarugale401@gmail.com>
2024-04-09 08:34:56 +0530

    useEffect added

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,12 +1,24 @@
 import RestorantCard from "./RestaurantCard";
-import { resobj } from "../utils/mockData"
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 // sending key as a index is not recommneded for looping over a component. becouse the index of perticuler value may change.
 const Body = () => {
     // state varibale = react hooks -> useState which is like ref in vue
-    const [listOfRestaurants, setlistOfRestaurants] = useState(resobj)
-    return (
+    const [listOfRestaurants, setlistOfRestaurants] = useState([])
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.4951012&lng=73.8051458&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        const json = await data.json()
+        console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
+        setlistOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+
+    return listOfRestaurants.length === 0 ? (<Shimmer />) : (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={() => {
---
+++ src/Components/Shimmer.js
@@ -0,0 +1,9 @@
+const Shimmer = () => {
+    return (
+        <div className="shimmer-container">
+            <img src="https://miro.medium.com/v2/resize:fit:4800/format:webp/1*VN-Fijz8nHBcl9hs6kCeUw.gif" width={100 + "%"} />
+        </div>
+    )
+}
+
+export default Shimmer;
